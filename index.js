28c139b57ca252737795e46db9becc84cca2f2f2
FrannkGv <fjgvpancho@gmail.com>
2021-03-17 21:03:43 -0600

    pokedex added

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
 const express = require('express')
 const app = express();
 
-/* 
-verbos HTTP
-GET
-POST
-PATH
-PUT
-DELETE
-*/
+const { pokemon } = require('./pokedex.json');
 
 app.get("/", (req, res, next) => {
     res.status(200);
-    res.send("Welcome!")
+    res.send("Bienvenido al Pokedex");
 });
 
-app.listen(3000, () =>{
+app.get("/pokemon/all", (req, res, next) =>{
+    res.status(200);
+    res.send(pokemon);
+});
+
+app.get('/pokemon/:id([0-9]{1,3})', (req, res, next) =>{
+    const id = req.params.id -1;
+    if(id >= 0 && id <= 150){
+        res.status(200);
+        res.send(pokemon[req.params.id -1]);
+    }
+    else{
+        res.status(404);
+        res.send("Pokemon no encontrado!");
+    }
+});
+
+app.get('/pokemon/:name', (req, res, next) =>{
+    const name = req.params.name;
+    for (let i = 0; i < pokemon.length; i++) {
+        if(pokemon[i].name  == name){
+            res.status(200);
+            res.send(pokemon[i]);
+        }
+    }
+    res.status(404);
+    res.send("Pokemon no encontrado");
+});
+
+app.listen(process.env.PORT || 3000, () =>{
     console.log("Server is running");
 });
