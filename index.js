8d25f0d49540ee12e1ad03d8b068ba4a528f0005
FrannkGv <fjgvpancho@gmail.com>
2021-04-27 20:38:54 -0500

    implemented JWT

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
+//Dependecies
 const morgan = require('morgan');
-const express = require('express')
+const express = require('express');
 const app = express();
-const pokemon = require('./routes/pokemon.js')
-const user = require('./routes/user.js')
+//routes
+const pokemon = require('./routes/pokemon.js');
+const user = require('./routes/user.js');
+//middleware
+const auth = require('./middleware/auth');
+const notFound = require('./middleware/notFound');
+const index = require('./middleware/index');
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.get("/", (req, res, next) => {
-    res.status(200).json({code: 1, message: "Bienvenido al pokedex"});
-});
-
+app.get("/", index);
 app.use("/user", user);
+app.use(auth);
 app.use("/pokemon", pokemon);
-
-app.use((req,res,next)=>{
-    return res.status(404).json({ code: 404, message: "URL no encontrada" }); 
-});
+app.use(notFound);
 
 app.listen(process.env.PORT || 3000, () =>{
     console.log("Server is running");
--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const user = express.Router();
 const db = require('../config/database');
 const jwt = require('jsonwebtoken');
 
-user.post("/", async (req, res, next)=>{
+user.post("/signin", async (req, res, next)=>{
     const {user_name, user_mail, user_password} = req.body;
 
     if(user_name && user_mail && user_password){  
